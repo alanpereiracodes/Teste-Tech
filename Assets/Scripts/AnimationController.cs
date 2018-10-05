1e277acc444e3b154c02cf4bfb5a94c63f3688a6
Alan Pereira <alan.registro@gmail.com>
2018-10-05 16:43:55 -0300

    Initial commit

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimationController : MonoBehaviour {
+
+    //
+    public Text nowPlayingText;
+
+    //
+    private Animator characterAnim;
+
+    private void Awake()
+    {
+        //Get Animator Controller of the Character Game Object in the Hierarchy.
+        characterAnim = GameObject.FindWithTag("Player").GetComponentInChildren<Animator>();
+
+        //Set the State for the default 0 (Idle).
+        characterAnim.SetInteger("animState", 0);
+
+    }
+
+    public void ChangeAnimationState(int stateNumber)
+    {
+        //Clamp the value to fit the current number of animation states.
+        stateNumber = Mathf.Clamp(stateNumber, 0, 5);
+
+        //Change the animaiton state in the animator.
+        characterAnim.SetInteger("animState", stateNumber);
+
+        //Change the Text for the current Animation
+        nowPlayingText.text = "Now Playing:  ";
+
+        switch(stateNumber)
+        {
+            case 0:
+                nowPlayingText.text += "Idle";
+                break;
+            case 1:
+                nowPlayingText.text += "Walk";
+                break;
+            case 2:
+                nowPlayingText.text += "Run";
+                break;
+            case 3:
+                nowPlayingText.text += "Jump";
+                break;
+            case 4:
+                nowPlayingText.text += "Physical Special";
+                break;
+            case 5:
+                nowPlayingText.text += "Magical Special";
+                break;
+        }
+
+    }//END Change Animation State function.
+
+}//END Animation Controller class.
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject sideCamera, topCamera;
+    public Text currentCameraText;
+
+    //Changes the camera based in the following values: 0 = Main, 1 = Side and 2 = Top.
+    public void ChangeCamera(int cameraNum)
+    {
+        //Clamps the value to avoid any issue.
+        cameraNum = Mathf.Clamp(cameraNum, 0, 2);
+
+        currentCameraText.text = "Current Camera:  ";
+
+        switch(cameraNum)
+        {
+            case 0:
+                currentCameraText.text += "Main";
+                sideCamera.SetActive(false);
+                topCamera.SetActive(false);
+                break;
+            case 1:
+                currentCameraText.text += "Side";
+                sideCamera.SetActive(true);
+                topCamera.SetActive(false);
+                break;
+            case 2:
+                currentCameraText.text += "Top";
+                sideCamera.SetActive(false);
+                topCamera.SetActive(true);
+                break;
+
+        }
+    }//END Change Camera function.
+
+}//END Camera Controller class.
